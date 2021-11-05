c037abb0351d27e1a79711681268b53e48619a53
Shirisha Madas <shirisha@monocept.com>
2021-11-05 19:11:02 +0530

    first commit

---
+++ VeggieSub.java
@@ -0,0 +1,43 @@
+public class VeggieSub extends Sandwich{
+	
+	// BAD WAY
+	/*
+	private boolean customerWantsCondiments = true;
+		
+	VeggieSub(boolean wantsCondiments){
+			
+		customerWantsCondiments = wantsCondiments;
+			
+	}
+		
+	public void makeSandwich(){
+			
+		cutBun();
+				
+		addVegetables();
+			
+		if(customerWantsCondiments()){
+			
+			addCondiments();
+			
+		}
+			
+		wrapSandwich();
+			
+	}
+	*/
+	
+	boolean customerWantsMeat(){ return false; }
+
+	void addMeat() {
+		
+	}
+
+
+	void addCondiments() {
+		
+		System.out.println("Vinegar and Oil Added");
+		
+	}
+
+}
